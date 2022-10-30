d53593530ee1a7841bf90d4f248efe88c4fa5f06
RemyXVX <remyfranciscoflores@gmail.com>
2022-10-30 03:19:12 -0700

    Created a business logic, that in thoery, will push our API to UI

--- src/index.js
+++ src/index.js
@@ -2,3 +2,26 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 
+
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+
+import Currency from './currency';
+
+async function exchangeRate(currencyOne, currencyTwo, money) {
+  const response = await Currency.exchangeRate(currencyOne, currencyTwo, money);
+  if (response.result === "success") {
+    printElems(response);
+  } else {
+    printError(response);
+  }
+}
+
+function printElems(response, currencyOne, currencyTwo, money) {
+  document.querySelector('#forNowResponse').innerText = `${currencyOne} ${money} is ${response["conversion_result"]} ${currencyTwo}`
+}
+
+function printError(error, currencyOne, currencyTwo, money) {
+  document.querySelector('#forNowResponse').innerText = `Computing too hard. Could not get ${currencyOne} to ${currencyTwo} to get any ${money}. Try again ${error}.`
+}
