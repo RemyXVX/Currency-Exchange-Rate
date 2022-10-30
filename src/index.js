4961bfb82869d8a0e3b11cc33013398068b79806
RemyXVX <remyfranciscoflores@gmail.com>
2022-10-30 03:32:49 -0700

    Refactor and added a function to handle values

--- src/index.js
+++ src/index.js
@@ -18,10 +18,22 @@ async function exchangeRate(currencyOne, currencyTwo, money) {
   }
 }
 
-function printElems(response, currencyOne, currencyTwo, money) {
+printElems = (response, currencyOne, currencyTwo, money) => {
   document.querySelector('#forNowResponse').innerText = `${currencyOne} ${money} is ${response["conversion_result"]} ${currencyTwo}`
 }
 
-function printError(error, currencyOne, currencyTwo, money) {
+printError = (error, currencyOne, currencyTwo, money) => {
   document.querySelector('#forNowResponse').innerText = `Computing too hard. Could not get ${currencyOne} to ${currencyTwo} to get any ${money}. Try again ${error}.`
 }
+
+submitionForm = (event) => {
+  event.preventDefault();
+  let currencyOne = document.getElementById('number1').value;
+  document.getElementById('number1').value = null;
+  let currencyTwo = document.getElementById('number2').value;
+  document.getElementById('number2').value = null;
+  let money = document.getElementById('cashMoney').value;
+  document.getElementById('cashMoney').value = null;
+  exchangeRate(currencyOne, currencyTwo, money);
+}
+
